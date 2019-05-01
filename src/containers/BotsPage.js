29284a1ad6efb72b6f437b71ca35e84be6775631
github username <github email address>
2019-05-01 10:38:45 -0400

    hit deliverables

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -22,44 +22,55 @@ class BotsPage extends React.Component {
 
     addArmyBot = (clickedBot) => {
 
+      // this.state.botsInArmy.includes(clickedBot) ?
+      // null
+      // :
+      // this.setState(prevState => ({
+      //   botsInArmy: [...prevState.botsInArmy, clickedBot]
+      // }))
+      const updateBots = this.state.bots.map(bot => {
+        if(bot === clickedBot){
+        return {...bot, inArmy: true}
+        } else {
+        return bot
+        }
+      })
       this.state.botsInArmy.includes(clickedBot) ?
       null
       :
-      this.setState(prevState => ({
-        botsInArmy: [...prevState.botsInArmy, clickedBot]
-      }))
-      // const updateBots = this.state.bots.map(bot => {
-      //   if(bot === clickedBot){
-      //   return {...bot, inArmy: true}
-      //   } else {
-      //   return bot
-      //   }
-      // })
+      this.setState({
+        bots: updateBots
+      })
     }
 
-    // renderArmyBots = () => {
-    //   const armyBots = this.state.bots.filter(bot => bot.inArmy === true)
-    // }
+    renderArmyBots = () => {
+      const armyBots = this.state.bots.filter(bot => bot.inArmy === true)
+      return armyBots
+    }
 
     removeArmyBot = (clickedBot) => {
-      const removeBot = this.state.botsInArmy.filter(bot => bot !== clickedBot)
+      // const removeBot = this.state.botsInArmy.filter(bot => bot !== clickedBot)
+      // this.setState(prevState => ({
+      //   botsInArmy: removeBot
+      // }))
+      const updateBots = this.state.bots.map(bot => {
+        if(bot === clickedBot){
+        return {...bot, inArmy: false}
+        } else {
+        return bot
+        }
+      })
       this.setState(prevState => ({
-        botsInArmy: removeBot
+        bots: updateBots
       }))
-      // const updateBots = this.state.bots.map(bot => {
-      //   if(bot === clickedBot){
-      //   return {...bot, inArmy: false}
-      //   } else {
-      //   return bot
-      //   }
-      // })
     }
 
   render() {
+    console.log();
     return (
       <div>
         < YourBotArmy
-        bots={this.state.botsInArmy}
+        bots={this.renderArmyBots()}
         removeArmyBot={this.removeArmyBot}
         botsInArmy={this.state.botsInArmy}
         />
