79a4fa4cd6f194fb7b87995bf4cf7fcfa725ba66
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-05-01 10:51:03 -0400

    completed second part

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,20 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '../components/BotSpecs';
 
 class BotCollection extends React.Component {
 	//your code here
 
 	render() {
-		const { bots } = this.props
+		const { bots, selectedBot } = this.props
 		return (
 			<div className="ui four column grid">
 				<div className="row">
-					{bots.map(bot => <BotCard bot={bot} key={bot.id} handleBot={this.props.handleBot} />)}
+					{selectedBot ? <BotSpecs
+						bot={selectedBot}
+						handleBack={this.props.handleBack}
+						handleEnlist={this.props.handleEnlist} />
+						: bots.map(bot => <BotCard bot={bot} key={bot.id} handleBot={this.props.handleBot} />)}
 				</div>
 			</div>
 		);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -7,7 +7,8 @@ class BotsPage extends React.Component {
 
   state = {
     bots: [],
-    army: []
+    army: [],
+    selectedBot: ''
   }
 
   componentDidMount() {
@@ -26,24 +27,36 @@ class BotsPage extends React.Component {
   }
 
   handleBot = (e, id) => {
-    const botsArray = [...this.state.bots]
-    const botsArmy = [...this.state.army]
-    const selectedBot = botsArray.find(bot => bot.id === id)
 
-    if (botsArray.find(bot => bot.id === id) && botsArmy.find(bot => bot.id === id)) {
+    const botsArmy = [...this.state.army]
+    const botsArray = [...this.state.bots]
+    const selectedBot = botsArray.find(bot => bot.id === id)
+    // this.setState({ selectedBot: selectedBot })
+
+    if (botsArmy.find(bot => bot.id === id)) {
       const idx = botsArmy.findIndex(bot => bot.id === id)
-      selectedBot.selected = false
-      const mainIdx = botsArray.findIndex(bot => bot.id === id)
-      botsArray[mainIdx] = selectedBot
       botsArmy.splice(idx, 1)
-      this.setState({ bots: botsArray, army: botsArmy })
-    } else if (selectedBot.selected !== true) {
-      const status = selectedBot.selected
-      selectedBot.selected = !status
-      const idx = botsArray.findIndex(bot => bot.id === id)
-      botsArray[idx] = selectedBot
-      this.setState({ bots: botsArray }, this.renderArmy)
+      this.setState({ army: botsArmy })
+
+    } else {
+      this.setState({ selectedBot: selectedBot })
     }
+
+
+    //   if (botsArray.find(bot => bot.id === id) && botsArmy.find(bot => bot.id === id)) {
+    //     const idx = botsArmy.findIndex(bot => bot.id === id)
+    //     selectedBot.selected = false
+    //     const mainIdx = botsArray.findIndex(bot => bot.id === id)
+    //     botsArray[mainIdx] = selectedBot
+    //     botsArmy.splice(idx, 1)
+    //     this.setState({ bots: botsArray, army: botsArmy })
+    //   } else if (selectedBot.selected !== true) {
+    //     const status = selectedBot.selected
+    //     selectedBot.selected = !status
+    //     const idx = botsArray.findIndex(bot => bot.id === id)
+    //     botsArray[idx] = selectedBot
+    //     this.setState({ bots: botsArray, selectedBot: selectedBot }, this.renderArmy)
+    //   }
   }
 
   renderArmy = () => {
@@ -52,13 +65,38 @@ class BotsPage extends React.Component {
     this.setState({ army: army })
   }
 
+  handleBack = (e) => {
+    e.preventDefault()
+    this.setState({ selectedBot: '' })
+
+  }
+
+  handleEnlist = (e, id) => {
+    e.preventDefault()
+    const botsArray = [...this.state.bots]
+    const botsArmy = [...this.state.army]
+
+    const selectedBot = botsArray.find(bot => bot.id === id)
+    botsArmy.push(selectedBot)
+    this.setState({ bots: botsArray, army: botsArmy, selectedBot: '' })
+
+  }
+
+
 
   render() {
-    const { bots, army } = this.state
+    const { bots, army, selectedBot } = this.state
     return (
       <div>
-        <YourBotArmy army={army.length > 0 ? army : []} handleBot={this.handleBot} />
-        <BotCollection bots={bots} handleBot={this.handleBot} />
+        <YourBotArmy
+          army={army.length > 0 ? army : []}
+          handleBot={this.handleBot} />
+        <BotCollection
+          bots={bots}
+          handleEnlist={this.handleEnlist}
+          handleBot={this.handleBot}
+          selectedBot={selectedBot}
+          handleBack={this.handleBack} />
       </div>
     );
   }
