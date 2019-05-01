da1064a9f3d89e1da85cd2ca6825c3334a6f8d7c
github username <github email address>
2019-05-01 10:31:04 -0400

    hit deliverables

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,21 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
+  renderBots = () => this.props.bots.map(bot => {
+    return <BotCard
+    bot={bot}
+    key={bot.id}
+    handleClick={this.props.addArmyBot}
+    botsInArmy={this.props.botsInArmy}
+    />
+  })
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.renderBots()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,73 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+    state = {
+      bots: [],
+      botsInArmy: []
+    }
+
+    componentDidMount(){
+        fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+        .then(res=>res.json())
+        .then(bots => {
+          const updatedBots = bots.map(bot => ({...bot, inArmy: false}))
+          this.setState({
+            bots: updatedBots
+          })
+        })
+    }
+
+    addArmyBot = (clickedBot) => {
+
+      this.state.botsInArmy.includes(clickedBot) ?
+      null
+      :
+      this.setState(prevState => ({
+        botsInArmy: [...prevState.botsInArmy, clickedBot]
+      }))
+      // const updateBots = this.state.bots.map(bot => {
+      //   if(bot === clickedBot){
+      //   return {...bot, inArmy: true}
+      //   } else {
+      //   return bot
+      //   }
+      // })
+    }
+
+    // renderArmyBots = () => {
+    //   const armyBots = this.state.bots.filter(bot => bot.inArmy === true)
+    // }
+
+    removeArmyBot = (clickedBot) => {
+      const removeBot = this.state.botsInArmy.filter(bot => bot !== clickedBot)
+      this.setState(prevState => ({
+        botsInArmy: removeBot
+      }))
+      // const updateBots = this.state.bots.map(bot => {
+      //   if(bot === clickedBot){
+      //   return {...bot, inArmy: false}
+      //   } else {
+      //   return bot
+      //   }
+      // })
+    }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        < YourBotArmy
+        bots={this.state.botsInArmy}
+        removeArmyBot={this.removeArmyBot}
+        botsInArmy={this.state.botsInArmy}
+        />
+        < BotCollection
+        bots={this.state.bots}
+        addArmyBot={this.addArmyBot}
+        botsInArmy={this.state.botsInArmy}
+        />
       </div>
     );
   }
