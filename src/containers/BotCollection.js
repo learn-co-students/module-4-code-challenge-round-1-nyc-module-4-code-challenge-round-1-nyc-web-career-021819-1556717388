d4602dffcd747aad22000885ff4afab40a4d387c
faizaniq <github email address>
2019-05-01 11:05:44 -0400

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,11 +5,12 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+    let allBots = this.props.bots.map(bot => <BotCard bot={bot} key={bot.id} botHandler={this.props.botHandler} specs={this.props.specs}/>)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
     		  Collection of all bots
+          {allBots}
     		</div>
   	  </div>
   	);
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,24 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+  removeBot = (bot) => {
+    this.props.botHandler(bot)
+  }
+
   render(){
+    let botArmy = this.props.botArmy.map(bot => <BotCard bot={bot} key={bot.id} botHandler={() => this.removeBot(bot)}/>)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            {botArmy}
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
