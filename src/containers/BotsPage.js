088ac517104761750ad0e8f11ddea1304a7c9ddd
Kate Kim <katekim5809@gmail.com>
2019-05-01 11:05:12 -0400

    KateKim

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,71 @@
 import React from "react";
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy'
+
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state={
+    bots: [],
+    botArmy: []
+  }
+
+  getBots(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res=>res.json())
+    .then(data => {
+      let newbots = data.map(bot => {
+        return {...bot, myArmy: false}
+      })
+        this.setState({
+          bots: newbots
+        })
+    }
+    )
+  }
+
+  componentDidMount(){
+    this.getBots()
+  }
+
+  handleAddtoBotArmy=(bot)=>{
+    let clicked = this.state.bots.find(b => b.id === bot.id);
+    this.state.bots.map(b => {
+      if(b===clicked){
+        return b.myArmy=true
+      } else {
+        return b
+      }
+    });
+    let updateBotArmy = this.state.bots.filter( b => b.myArmy===true);
+    this.setState({
+      botArmy: updateBotArmy
+    })
+  }
+
+  // myArmy=()=>{
+  //   let updateBotArmy = this.state.bots.filter( b => b.myArmy===true);
+  //   console.log(updateBotArmy);
+  // }
+
 
   render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
+    // console.log(this.state.botArmy);
+    // if(this.state.botArmy === []){
+    //   return (
+    //   <div>
+    //     <BotCollection bots={this.state.bots}
+    //     handleAddtoBotArmy={this.handleAddtoBotArmy}/>
+    //   </div>
+    // );}else{
+      return (
+        <div>
+          <YourBotArmy botArmy={this.state.botArmy}/>
+          <BotCollection bots={this.state.bots}
+          handleAddtoBotArmy={this.handleAddtoBotArmy}/>
+        </div>
+      )
+    // }
   }
 
 }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -3,20 +3,24 @@ import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
   //your bot army code here...
+  renderBotArmy=()=>{
+    return this.props.botArmy.map(b => <BotCard key={b.id} bot={b}/>)
+  }
 
   render(){
+    console.log(this.props.botArmy);
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+          BotArmy List
+            {this.renderBotArmy()}
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
